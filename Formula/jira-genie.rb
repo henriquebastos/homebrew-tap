116019dcb931ac5cb97fcc97e5636ad687a616dd
Henrique Bastos <henrique@bastos.net>
2026-03-27 10:12:42 -0300

    jira-genie 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/jira-genie.rb
+++ Formula/jira-genie.rb
@@ -3,8 +3,8 @@ class JiraGenie < Formula
 
   desc "Your AI agent's interface to Jira Cloud"
   homepage "https://github.com/henriquebastos/jira-genie"
-  url "https://github.com/henriquebastos/jira-genie/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "430f6a06859f10418c793003b0873ef9398b7a12bdf19dc31f7b44937311b747"
+  url "https://github.com/henriquebastos/jira-genie/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "ba477375156230afbc76fbdd6f33227a1dea57d1370ceaea19d157c1fbdc3323"
   license "MIT"
 
   depends_on "python@3.13"
