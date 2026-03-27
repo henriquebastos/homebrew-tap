8e063df085a29eb66fef0aa2e637cc36eff05fe4
Henrique Bastos <henrique@bastos.net>
2026-03-27 10:20:06 -0300

    fix: use virtualenv_create + pip_install for jira-genie formula

--- Formula/jira-genie.rb
+++ Formula/jira-genie.rb
@@ -3,49 +3,16 @@ class JiraGenie < Formula
 
   desc "Your AI agent's interface to Jira Cloud"
   homepage "https://github.com/henriquebastos/jira-genie"
-  url "https://github.com/henriquebastos/jira-genie/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "eb175492d2717b50e195b751a9edf8c9cfc4a859551a2fb68855e61344f9bccd"
+  url "https://files.pythonhosted.org/packages/source/j/jira-genie/jira_genie-0.2.2.tar.gz"
+  sha256 :no_check
   license "MIT"
 
   depends_on "python@3.13"
 
-  resource "argcomplete" do
-    url "https://files.pythonhosted.org/packages/38/61/0b9ae6399dd4a58d8c1b1dc5a27d6f2808023d0b5dd3104bb99f45a33ff6/argcomplete-3.6.3.tar.gz"
-    sha256 "62e8ed4fd6a45864acc8235409461b72c9a28ee785a2011cc5eb78318786c89c"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/af/2d/7bf41579a8986e348fa033a31cdd0e4121114f6bce2457e8876010b092dd/certifi-2026.2.25.tar.gz"
-    sha256 "e887ab5cee78ea814d3472169153c2d12cd43b14bd03329a39a9c6e2e80bfba7"
-  end
-
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/7b/60/e3bec1881450851b087e301bedc3daa9377a4d45f1c26aa90b0b235e38aa/charset_normalizer-3.4.6.tar.gz"
-    sha256 "1ae6b62897110aa7c79ea2f5dd38d1abca6db663687c0b1ad9aed6f6bae3d9d6"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
-    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
-  end
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
-    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
-  end
-
-  resource "requests-pro" do
-    url "https://files.pythonhosted.org/packages/86/2e/c32114e4e1c47704f938eb99457adf09e5ed347bebcd83d078b37c9339c7/requests_pro-1.0.0.tar.gz"
-    sha256 "3fd751c3c6defa249bdca148063481ff7f40f4151a0c794aec857aca6e87e335"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
-    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
-  end
-
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.13")
+    venv.pip_install "jira-genie==#{version}"
+    bin.install_symlink Dir[libexec/"bin/jira"]
   end
 
   test do
